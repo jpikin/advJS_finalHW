1678ab99251175adcf940b8f25146bc1edecdf80
pikin <pikin@bk.ru>
2024-10-08 00:27:02 +0500

    save data to local storage added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const localStorage = window.localStorage;
+const sendBtn = document.querySelector('.send-btn');
+const prodTitle = document.querySelector('.title');
+const prodComment = document.querySelector('.text');
+let ID = 0;
+
+
+sendBtn.addEventListener('click', () => {
+    const title = prodTitle.value;
+    const comment = prodComment.value;
+    localStorage.setItem(ID++, JSON.stringify({title, comment}));
+    prodTitle.value = '';
+    prodComment.value = '';
+});
+
